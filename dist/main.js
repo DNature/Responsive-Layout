457939a8510500644a3b3eb4370e2742babb742d
Divine Nature <divinehycenth8@gmail.com>
2019-04-22 11:09:46 +0100

    Shop, Blog, Faq abd Contact page Completed and Responsive

--- dist/main.js
+++ dist/main.js
@@ -17,6 +17,21 @@ $(document).ready(function() {
 		$('.menu-btn').toggleClass('close');
 		$('nav').toggleClass('show');
 	});
+	$('.title').click(function() {
+		$('.fac-box').toggleClass('active');
+	});
+	$('.title1').click(function() {
+		$('.fac-box1').toggleClass('active');
+	});
+	$('.title2').click(function() {
+		$('.fac-box2').toggleClass('active');
+	});
+	$('.title3').click(function() {
+		$('.fac-box3').toggleClass('active');
+	});
+	$('.title4').click(function() {
+		$('.fac-box4').toggleClass('active');
+	});
 
 	$(this).bind('contextmenu', function(e) {
 		e.preventDefault();
